d293820ac4803829562dc516439bd1386427c2f6
gablecc <69671509+gablecc@users.noreply.github.com>
2021-06-07 14:26:19 +0200

    novo

--- js/script.js
+++ js/script.js
@@ -1,29 +1,3 @@
-/*$(document).ready(function(){
-    $('.next').on('click', function(){
-        var trenutnaSlika=$('.active');
-        var slijedecaSlika=trenutnaSlika.next();
-        
-        if(slijedecaSlika.length){
-            trenutnaSlika.removeClass('active').css('z-index',-10);
-            slijedecaSlika.addClass('active').css('z-index',10);
-        }
-        
-
-    });
-
-    $('.prev').on('click', function(){
-        var trenutnaSlika=$('.active');
-        var zadnjaSlika=trenutnaSlika.prev();
-
-        if(zadnjaSlika.length){
-            trenutnaSlika.removeClass('active').css('z-index',-10);
-            zadnjaSlika.addClass('active').css('z-index',10);
-            
-        }
-    });
-})
-*/
-
 "use strict";
 
 var slider_img = document.querySelector(".slider-img");
