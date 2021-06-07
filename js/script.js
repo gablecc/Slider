e066dd0eb8e4de352dd5426a5cf1b51135662bdb
gablecc <69671509+gablecc@users.noreply.github.com>
2021-06-07 14:24:49 +0200

    novo

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
     $('.next').on('click', function(){
         var trenutnaSlika=$('.active');
         var slijedecaSlika=trenutnaSlika.next();
@@ -22,3 +22,36 @@ $(document).ready(function(){
         }
     });
 })
+*/
+
+"use strict";
+
+var slider_img = document.querySelector(".slider-img");
+var images = [
+  "slider-image-1.jpg",
+  "slider-image-2.jpg",
+  "slider-image-3.jpg",
+  "slider-image-4.jpg",
+  "slider-image-5.jpg",
+  "slider-image-6.jpg",
+  "slider-image-7.jpg",
+  "slider-image-8.jpg",
+  "slider-image-9.jpg"
+];
+var i = 0;
+
+function prev() {
+  if (i <= 0) i = images.length;
+  i--;
+  return setImg();
+}
+
+function next() {
+  if (i >= images.length - 1) i = -1;
+  i++;
+  return setImg();
+}
+
+function setImg() {
+  return slider_img.setAttribute("src", "assets/" + images[i]);
+}
